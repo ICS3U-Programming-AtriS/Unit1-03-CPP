3a3c7e6125c338aa6a2a8d710fb100b901c0aede
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-02-12 04:19:57 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2025 Atri Sarker All rights reserved.
+// .
+// Created By: Atri S
+// Date: Feb 11, 2025
+// This program displays "Hello World" with style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Salutions, my fellow Earth!" << std::endl;
+}
